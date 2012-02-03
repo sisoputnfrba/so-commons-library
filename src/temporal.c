d6a05648d6f0bc5fe233205369a568e7be7e5520
Gaston Prieto <gaston.prieto.utn@gmail.com>
2012-02-03 14:12:32 -0300

    Add ADT Temporal

---
+++ src/temporal.c
@@ -0,0 +1,41 @@
+/*
+ * temporal.c
+ *
+ *  Created on: Feb 3, 2012
+ *      Author: Prieto, Gaston
+ */
+
+#include "temporal.h"
+#include "error_operations.h"
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/timeb.h>
+
+t_string temporal_get_string_time() {
+	time_t log_time;
+	struct tm *log_tm;
+	struct timeb tmili;
+	t_string str_time = malloc(8 + 1);
+
+	if ((log_time = time(NULL)) == -1) {
+		error_show("Error getting date!");
+		return 0;
+	}
+
+	log_tm = localtime(&log_time);
+
+	if (ftime(&tmili)) {
+		error_show("Error getting time!");
+		return 0;
+	}
+
+	t_string partial_time = malloc(8 + 1);
+	strftime(partial_time, 127, "%H:%M:%S", log_tm);
+	sprintf(str_time, "%s:%hu", partial_time, tmili.millitm);
+	string_destroy(partial_time);
+
+	return str_time;
+}
---
+++ src/temporal.h
@@ -0,0 +1,15 @@
+/*
+ * temporal.h
+ *
+ *  Created on: Feb 3, 2012
+ *      Author: Prieto, Gaston
+ */
+
+#ifndef TEMPORAL_H_
+#define TEMPORAL_H_
+
+#include "string_operations.h"
+
+t_string temporal_get_string_time();
+
+#endif /* TEMPORAL_H_ */
