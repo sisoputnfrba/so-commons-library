4d91027dd736f9185fca61db833dda54a4a8f675
Gaston Prieto <gaston.prieto.utn@gmail.com>
2012-02-03 14:12:02 -0300

    Add ADT Error operations

---
+++ src/error_operations.c
@@ -0,0 +1,23 @@
+/*
+ * error_operations.c
+ *
+ *  Created on: Feb 3, 2012
+ *      Author: Prieto, Gaston
+ */
+
+
+#include "error_operations.h"
+
+#include <stdarg.h>
+
+void error_show(t_string message, ...) {
+	va_list arguments;
+	va_start(arguments, message);
+
+	t_string error_message = string_new("[[ERROR]]");
+	error_message = string_append(error_message, message);
+
+	vprintf(error_message, arguments);
+
+	va_end(arguments);
+}
---
+++ src/error_operations.h
@@ -0,0 +1,15 @@
+/*
+ * error_operations.h
+ *
+ *  Created on: Feb 3, 2012
+ *      Author: Prieto, Gaston
+ */
+
+#ifndef ERROR_OPERATIONS_H_
+#define ERROR_OPERATIONS_H_
+
+#include "string_operations.h"
+
+void error_show(t_string message, ...);
+
+#endif /* ERROR_OPERATIONS_H_ */
