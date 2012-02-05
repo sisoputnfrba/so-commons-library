74359e3d4050120c916ecdf06892bf4b70f9e207
Gaston Prieto <gaston.prieto.utn@gmail.com>
2012-02-05 05:19:27 -0300

    Add examples for ADT String

---
+++ examples/example-string-operactions.c
@@ -0,0 +1,22 @@
+/*
+ * example-string-operactions.c
+ *
+ *  Created on: Feb 3, 2012
+ *      Author: shinichi
+ */
+
+#include <string_operations.h>
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	t_string string = string_new("hola");
+	string_append(&string, " ");
+	string_append(&string, "Mundo!");
+	puts(string);
+	printf("Is equal: %s",
+			string_equals_ignore_case(string, "HOLA MUNDO!") ? "True" : "False");
+	string_destroy(string);
+	return 0;
+}
+
