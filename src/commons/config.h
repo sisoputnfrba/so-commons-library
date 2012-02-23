d85544c77910e13e25b139dc1c542ef9e459e2ee
Facundo Viale <facundo.m.viale@gmail.com>
2012-02-22 22:59:54 -0300

    Refactoring de biblioteca de config

--- src/commons/config.h
+++ src/commons/config.h
@@ -17,10 +17,10 @@
 	} t_config;
 
 	t_config *config_create(char *path);
-	char *config_get_string_value(t_config*, char *key);
-	int config_get_int_value(t_config*, char *key);
-	long config_get_long_value(t_config*, char *key);
-	double config_get_double_value(t_config*, char *key);
-	void config_destroy(t_config *config);
+	char 	 *config_get_string_value(t_config*, char *key);
+	int 	  config_get_int_value(t_config*, char *key);
+	long	  config_get_long_value(t_config*, char *key);
+	double 	  config_get_double_value(t_config*, char *key);
+	void 	  config_destroy(t_config *config);
 
 #endif /* CONFIG_H_ */
