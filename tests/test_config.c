f2c0153d1b545fc9f88b7f7ed68f4b02e48758e4
Gaston Prieto <gaston.prieto.utn@gmail.com>
2012-02-24 02:03:56 -0300

    Cambio los asserts en el test de la configuracion

--- tests/test_config.c
+++ tests/test_config.c
@@ -29,18 +29,18 @@ static int clean_suite() {
 }
 
 void test_read_config() {
-	t_config* config = config_create("tests/resources/config.cfg");
+	t_config* config = config_create("resources/config.cfg");
 
-	CU_ASSERT_EQUAL_FATAL(config_keys_amount(config), 4);
+	CU_ASSERT_EQUAL(config_keys_amount(config), 4);
 
-	CU_ASSERT_TRUE_FATAL(config_has_property(config, "IP"));
+	CU_ASSERT_TRUE(config_has_property(config, "IP"));
 	CU_ASSERT_STRING_EQUAL(config_get_string_value(config, "IP"), "127.0.0.1");
 
-	CU_ASSERT_TRUE_FATAL(config_has_property(config, "PORT"));
-	CU_ASSERT_EQUAL_FATAL(config_get_int_value(config, "PORT"), 8080);
+	CU_ASSERT_TRUE(config_has_property(config, "PORT"));
+	CU_ASSERT_EQUAL(config_get_int_value(config, "PORT"), 8080);
 
-	CU_ASSERT_TRUE_FATAL(config_has_property(config, "LOAD"));
-	CU_ASSERT_EQUAL_FATAL(config_get_double_value(config, "LOAD"), 0.5);
+	CU_ASSERT_TRUE(config_has_property(config, "LOAD"));
+	CU_ASSERT_EQUAL(config_get_double_value(config, "LOAD"), 0.5);
 
 	config_destroy(config);
 }
